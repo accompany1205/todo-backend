3201ca6d24c23dc5eddc940abf549c6d0c6767f4
accompany1205 <accompaniedwithyou@gmail.com>
2023-12-18 09:24:57 -0800

    changed http to https

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,9 +6,4 @@ var router = express.Router();
 
 router.use("/auth", authRouter).use("/task", taskRouter);
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "asdf" });
-});
-
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,23 +1,31 @@
 // Using ESM syntax for environment variables
-import 'dotenv/config';
-import express from 'express';
-import mongoose from 'mongoose';
-import session from 'express-session';
-import passport from 'passport';
-import cors from 'cors';
-import indexRouter from './routes/index.js';
-import passportConfig from './config/passport.js';
-import keys from './config/key.js';
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import session from "express-session";
+import passport from "passport";
+import cors from "cors";
+import https from "https";
+import fs from 'fs';
+import path from 'path';
+import indexRouter from "./routes/index.js";
+import passportConfig from "./config/passport.js";
+import keys from "./config/key.js";
 
 // Database Connection
 const DATABASE_URL = keys.mongoURI;
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 mongoose
   .connect(DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('MongoDB connected…'))
+  .then(() => console.log("MongoDB connected…"))
   .catch((err) => console.error(err));
+const SSL_CERT_PATH = path.resolve(__dirname, '../server.crt');
+console.log(SSL_CERT_PATH);
+const SSL_KEY_PATH = path.resolve(__dirname, '../server.key');
 
 const app = express();
 
@@ -30,13 +38,17 @@ app.use(passport.session());
 app.use(cors());
 
 // Use routers
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // Passport config
 passportConfig(passport);
 
 // Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 8000;
+const httpsOptions = {
+  key: fs.readFileSync(SSL_KEY_PATH),
+  cert: fs.readFileSync(SSL_CERT_PATH),
+};
+https.createServer(httpsOptions, app).listen(PORT, () => {
+  console.log(`HTTPS server running on port ${PORT}`);
 });
